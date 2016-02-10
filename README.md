7583f12c2eda48da7c028c389a89abc8b6813c4b
giorger <ggerakakis@gmail.com>
2016-02-11 01:46:08 +0200

    Main functionality complete

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-Python API for Runtastic. Code used as baseline is https://github.com/timoschlueter/php-runtastic
+## Synopsis
+Python API for Runtastic. 
+Code used as baseline is https://github.com/timoschlueter/php-runtastic
+
+## Code Example
+TODO
+
+## Motivation
+Creating a python API 
+
+## API Reference
+####### Data Model
+The following data model has been implemented in order to store required information:
+
+- Dictionary __sessions
+    - key:random_uuid (created within class)
+      - value: dictionary
+        - *key*: **username**           
+            - *value*: **runtastic username**
+        - *key*: **userid**             
+            - *value*: **runtastic user id**
+        - *key*: **authenticity_token** 
+            - *value*: **runtastic authenticity token generated upon login**
+        - *key*: **sport_sessions**     
+            - *value*: **dictionary (containing json records for each session)**
+                - *key*: **id**           
+                   - *value*: **runtastic sport session id**
+
+## Tests
+TODO
+
+
+## Installation
+Download, change the values for Runtastic username/password and execute
+
+## License
+The MIT License (MIT)
+
+Copyright (c) 2016 giorger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- python-runtastic.py
+++ python-runtastic.py
@@ -1,3 +1,27 @@
+'''
+The MIT License (MIT)
+
+Copyright (c) 2016 giorger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 import ConfigParser
 import httplib
 import json
@@ -15,13 +39,12 @@ class ConfigurationObject:
     def get_config_value(self, c_area, c_name):
         return self.conf_file.get(c_area, c_name)
 
-
 class Runtastic:
     __config = 0
     __sessions = {}
 
     def __init__(self):
-        Runtastic.__config = ConfigurationObject("python-runtastic.local")
+        Runtastic.__config = ConfigurationObject("python-runtastic.ini")
 
     def login(self, session_uuid):
         if Runtastic.__sessions.has_key(session_uuid):
@@ -62,7 +85,6 @@ class Runtastic:
                                                                            "field_token"): self.authenticity_token}
                 self.sessionid = str(uuid.uuid4())
                 Runtastic.__sessions[self.sessionid] = self.sessionDetails
-                print(str(self.responseOutputJson['current_user']['id']) + " " + str(self.authenticity_token))
                 return self.sessionid
             else:
                 print ("Error:" + self.conn_response.reason)
@@ -72,7 +94,6 @@ class Runtastic:
         if Runtastic.__sessions.has_key(session_uuid):
             self.conn = httplib.HTTPSConnection(Runtastic.__config.get_config_value("runtastic", "runtasticUrl"))
             self.conn.request("GET", Runtastic.__config.get_config_value("runtastic", "logoutUrl"))
-            print (Runtastic.__config.get_config_value("runtastic", "logoutUrl"))
             self.conn_response = self.conn.getresponse()
             if self.conn_response.status == 200 or self.conn_response.status == 302:
                 del Runtastic.__sessions[session_uuid]
@@ -98,10 +119,8 @@ class Runtastic:
             if self.conn_response.status == 200:
                 self.dataa = re.search("var index_data = (.*)\;", self.conn_response.read()).group()
                 self.datab = re.search("\[\[.*\]\]", self.dataa).group()
-                print (self.datab)
                 self.session_data_json = json.loads(self.datab)
                 self.listsess = ",".join(str(self.keyss[0]) for self.keyss in self.session_data_json)
-                print self.listsess
                 self.values = {Runtastic.__config.get_config_value("runtastic",
                                                                    "header_user_id"):
                                    (Runtastic.__sessions[session_uuid])[
@@ -118,14 +137,18 @@ class Runtastic:
                                    self.data2, self.headers2)
                 self.conn2_response = self.conn2.getresponse()
                 if self.conn2_response.status == 200:
+                    self.all_sport_sessions = {}
                     self.responseOutputJson2 = json.loads(self.conn2_response.read())
-                    print (self.responseOutputJson2)
+                    for self.session in self.responseOutputJson2:
+                        self.all_sport_sessions[self.session['id']] = self.session
+                    (Runtastic.__sessions[session_uuid])[Runtastic.__config.get_config_value("python-runtastic",
+                                                                                             "field_sport_sessions")] \
+                        = self.all_sport_sessions
         else:
             print ("Error")
 
 
 myConnection = Runtastic()
 mySessionId = myConnection.login('')
-print mySessionId
 myConnection.retrieve_all_sessions(mySessionId)
-# myConnection.logout(mySessionId)
+myConnection.logout(mySessionId)
